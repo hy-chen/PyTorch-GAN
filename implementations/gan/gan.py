7c146b295c8c98f2327bc6cb9c9716e29b9a5930
Charlie <47866975+hy-chen@users.noreply.github.com>
2021-03-05 09:59:55 -0500

    study notes

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -28,61 +28,66 @@ parser.add_argument("--img_size", type=int, default=28, help="size of each image
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
 opt = parser.parse_args()
-print(opt)
+print(opt) # not args, but opt. 
 
-img_shape = (opt.channels, opt.img_size, opt.img_size)
+img_shape = (opt.channels, opt.img_size, opt.img_size) # square
 
 cuda = True if torch.cuda.is_available() else False
 
 
-class Generator(nn.Module):
+class Generator(nn.Module): # generator is an nn.module
+    """essentially, generator take a batch of latent space embeddings and use fc with different acts to map them into a batch of images. """
     def __init__(self):
         super(Generator, self).__init__()
-
+        """
+        it has some blocks and a fc at the end, with a tanh act. 
+        the blocks are just fc as well, with optional 1d bn, and leakyrelu act.
+        """
         def block(in_feat, out_feat, normalize=True):
             layers = [nn.Linear(in_feat, out_feat)]
             if normalize:
                 layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(nn.LeakyReLU(0.2, inplace=True)) # leaky relu is not zero when x <=0, it is -ax. where a is an input arg, like 0.2 here)
             return layers
 
         self.model = nn.Sequential(
-            *block(opt.latent_dim, 128, normalize=False),
+            *block(opt.latent_dim, 128, normalize=False), # opt.latent_dim is the in_features. 
             *block(128, 256),
             *block(256, 512),
             *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
+            nn.Linear(1024, int(np.prod(img_shape))), 
+            nn.Tanh() # tanh is (-1,1) # sigmoid is (0,1)
         )
 
     def forward(self, z):
         img = self.model(z)
-        img = img.view(img.size(0), *img_shape)
+        img = img.view(img.size(0), *img_shape) # batch size unchanged, make the flat 1D to 3D for image. 
         return img
 
 
-class Discriminator(nn.Module):
+class Discriminator(nn.Module): 
+    """discriminator takes in a batch of images, map them into a batch of probabilty scalars"""
     def __init__(self):
         super(Discriminator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.Linear(int(np.prod(img_shape)), 512), # takes in images
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
+            nn.Linear(256, 1), # just one feature output?
+            nn.Sigmoid(), # sigmoid as output. (0,1)
         )
 
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
+        img_flat = img.view(img.size(0), -1) # flatten 
         validity = self.model(img_flat)
 
         return validity
 
 
 # Loss function
-adversarial_loss = torch.nn.BCELoss()
+adversarial_loss = torch.nn.BCELoss() # measures the Binary Cross Entropy between the target and the output.
 
 # Initialize generator and discriminator
 generator = Generator()
@@ -109,7 +114,7 @@ dataloader = torch.utils.data.DataLoader(
 )
 
 # Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2)) # generator and descriminator have their own optimizers
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
@@ -122,7 +127,7 @@ for epoch in range(opt.n_epochs):
     for i, (imgs, _) in enumerate(dataloader):
 
         # Adversarial ground truths
-        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False) # labels for each sample
         fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
         # Configure input
@@ -135,7 +140,7 @@ for epoch in range(opt.n_epochs):
         optimizer_G.zero_grad()
 
         # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim)))) # noise as generator input
 
         # Generate a batch of images
         gen_imgs = generator(z)
